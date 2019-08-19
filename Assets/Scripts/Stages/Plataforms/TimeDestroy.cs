b0d18f1b4fa9e10f65fcfffc266d95f83dd0ea08
Francisco Murillo <fmurillom@gmail.com>
2019-08-18 19:18:53 -0600

    [Add]WheatStage Scenery

---
+++ Assets/Scripts/Stages/Plataforms/TimeDestroy.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TimeDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float destroyTime;
+
+    private bool active = true;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!active)
+        {
+            active = true;
+
+
+            StartCoroutine(AppAfterTime(5));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+
+            StartCoroutine(DisAfterTime(1));
+        }
+    }
+
+    IEnumerator DisAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        // Code to execute after the delay
+
+        active = false;
+
+        gameObject.GetComponent<TilemapRenderer>().enabled = false;
+
+        gameObject.GetComponent<TilemapCollider2D>().enabled = false;
+
+    }
+
+    IEnumerator AppAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        // Code to execute after the delay
+
+        gameObject.GetComponent<TilemapRenderer>().enabled = true;
+
+        gameObject.GetComponent<TilemapCollider2D>().enabled = true;
+
+
+
+    }
+}
---
+++ Assets/Scripts/Stages/Stages/RestarterWheat.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestarterWheat : MonoBehaviour
+{
+    public float xpos;
+    public float ypos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            rb.velocity = Vector2.zero;
+            rb.angularVelocity = 0f;
+
+            collision.gameObject.transform.position = new Vector2(xpos, ypos);
+            SceneManager.LoadScene("WheatStage");
+        }
+    }
+}
